431459555037eeeec5b028faa77e594060240a9f
Julien Kauffmann <julien.kauffmann@freelan.org>
2019-05-05 14:20:05 -0400

    Added basic skeleton for prometheus exporter

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/intelux/hubitat
 
 go 1.12
+
+require (
+	github.com/intelux/insteon v0.0.0-20190504004335-c49e5fdacc47 // indirect
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3 // indirect
+)
---
+++ hubitat-prometheus-exporter/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/intelux/hubitat"
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCtx, rootCtxCancel = withInterrupt(context.Background())
+	endpoint               = ":8081"
+)
+
+var rootCmd = &cobra.Command{
+	Use:  "hubitat-prometheus-exporter",
+	Args: cobra.NoArgs,
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		rootCtxCancel()
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client, err := hubitat.NewClientFromEnv()
+
+		if err != nil {
+			return fmt.Errorf("instanciating Hubitat client: %s", err)
+		}
+
+		fmt.Fprintf(cmd.OutOrStderr(), "Listening on %s.\n", endpoint)
+
+		server := &http.Server{
+			Addr: endpoint,
+			Handler: &Handler{
+				Client: client,
+			},
+		}
+
+		go func() {
+			<-rootCtx.Done()
+
+			shutdownCtx, cancel := context.WithTimeout(context.Background(), time.Second)
+			server.Shutdown(shutdownCtx)
+			cancel()
+
+			server.Close()
+		}()
+
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			return err
+		}
+
+		fmt.Fprintf(cmd.OutOrStderr(), "Server shutdown.\n")
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.Flags().StringVarP(&endpoint, "endpoint", "l", endpoint, "the endpoint to listen on")
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+// Handler implements a handler.
+type Handler struct {
+	Client *hubitat.Client
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+}
+
+func withInterrupt(ctx context.Context) (context.Context, func()) {
+	ctx, cancel := context.WithCancel(ctx)
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	go func() {
+		<-ch
+		cancel()
+	}()
+
+	go func() {
+		<-ctx.Done()
+
+		signal.Stop(ch)
+		close(ch)
+	}()
+
+	return ctx, cancel
+}
